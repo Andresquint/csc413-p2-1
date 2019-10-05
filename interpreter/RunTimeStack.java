514013bc67cc3b84f7b4303287b2378b05927402
Corey Russ <cruss.seq11@gmail.com>
2019-10-05 11:44:46 -0700

    initial thoughts for implementing runtimestack

--- interpreter/RunTimeStack.java
+++ interpreter/RunTimeStack.java
@@ -15,30 +15,53 @@ public class RunTimeStack {
         // point of our language, so its frame pointer is 0.
         framePointer.add(0);
     }
-
     public void dump(){
 
     }
     public int peek() {
-        return 0;
+        if(runTimeStack.size() >= 1)
+            return runTimeStack.get(runTimeStack.size()-1);
+        else return -1;
     }
     public int push(int i){
-        return 0;
+        runTimeStack.add(i);
+        return i;
     }
     public int pop(){
-        return 0;
+        int i = -1;
+        if (runTimeStack.size() > 0)
+            i = runTimeStack.remove(runTimeStack.size());
+        return i;
     }
     public int store(int offset){
-        return 0;
+        if(offset + framePointer.peek() > runTimeStack.size() - 1 || offset < 0)
+        {
+            return -1;
+        }
+        else {
+            int p = pop();
+            runTimeStack.set(p, framePointer.peek() + offset);
+            return p;
+        }
     }
     public int load(int offset){
-        return 0;
+       // if(framePointer.peek() + offset < runTimeStack.size())
+        runTimeStack.add(runTimeStack.get(framePointer.peek() + offset));
+        return runTimeStack.get(framePointer.peek() + offset);
     }
     public void newFrameAt(int offset){
-
+        if(runTimeStack.size() - offset != runTimeStack.get(runTimeStack.size() - 1)){
+            framePointer.add(runTimeStack.size() - offset);
+        }
     }
     public void popFrame(){
-        
-    }
+        int topValue = runTimeStack.get(runTimeStack.size() - 1);
 
+        runTimeStack.subList(framePointer.peek(), runTimeStack.size());
+
+        if(framePointer.peek() != 0)
+            framePointer.pop();
+
+        runTimeStack.add(topValue);
+    }
 }
