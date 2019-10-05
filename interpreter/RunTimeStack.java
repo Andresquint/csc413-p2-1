7954e801659d0458fcce1f0b995abcf77967ca0e
Corey Russ <cruss.seq11@gmail.com>
2019-10-04 22:06:08 -0700

    add function shells

--- interpreter/RunTimeStack.java
+++ interpreter/RunTimeStack.java
@@ -15,5 +15,30 @@ public class RunTimeStack {
         // point of our language, so its frame pointer is 0.
         framePointer.add(0);
     }
-    
+
+    public void dump(){
+
+    }
+    public int peek() {
+        return 0;
+    }
+    public int push(int i){
+        return 0;
+    }
+    public int pop(){
+        return 0;
+    }
+    public int store(int offset){
+        return 0;
+    }
+    public int load(int offset){
+        return 0;
+    }
+    public void newFrameAt(int offset){
+
+    }
+    public void popFrame(){
+        
+    }
+
 }
