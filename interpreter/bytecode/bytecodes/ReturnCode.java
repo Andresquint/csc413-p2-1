b06ec32f643fe0ea6a44cadef0938429ab0b2aaa
scrable <cruss.seq11@gmail.com>
2019-10-04 19:52:40 -0700

    change type to string instead of char

--- interpreter/bytecode/bytecodes/ReturnCode.java
+++ interpreter/bytecode/bytecodes/ReturnCode.java
@@ -6,18 +6,17 @@ import java.util.ArrayList;
 
 public class ReturnCode extends ByteCode {
     private int returnValue;
-    private char id;
+    private String id;
 
     @Override
     public void initCode(ArrayList args) {
         try {
-            id = ((String)args.get(1)).charAt(0);
+            id = (String)args.get(1);
             if (args.size() == 3)
                 returnValue = Integer.parseInt((String) args.get(2));
         }
         catch(Exception e){e.printStackTrace();}
     }
-
     @Override
     public void execute() {
 
