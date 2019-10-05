00f0244196ed94a4f64f1adaf867bbc8ebf566ed
scrable <cruss.seq11@gmail.com>
2019-10-04 19:58:40 -0700

    theoretically implement resolveAddresses

--- interpreter/Program.java
+++ interpreter/Program.java
@@ -1,13 +1,21 @@
 package interpreter;
 
 import interpreter.bytecode.ByteCode;
+import interpreter.bytecode.bytecodes.CallCode;
+import interpreter.bytecode.bytecodes.FalseBranchCode;
+import interpreter.bytecode.bytecodes.GotoCode;
+import interpreter.bytecode.bytecodes.LabelCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Program {
 
     private ArrayList<ByteCode> program;
+
+    //stores the index (line number)  for each LABEL code
+    private HashMap<String, Integer> h = new HashMap<>();
 
     public Program() {
         program = new ArrayList<>();
@@ -24,13 +32,47 @@ public class Program {
      * HINT: make note what type of data-stucture bytecodes are stored in.
      */
     public void resolveAddrs() {
-
+        //loop through all lines and collect LABELs and their respective line numbers
+        //place label concatenated with number as key and line number as value
+        for(int i = 1; i <= program.size() - 1; i++)
+        {
+            if (program.get(i) instanceof LabelCode)
+            {
+                h.put(((LabelCode) program.get(i)).getLabel() + ((LabelCode) program.get(i)).getNumber(), i);
+            }
+        }
+        //replace the previous numbers with line numbers from hashmap
+        for(int i = 1; i <= program.size() - 1; i++)
+        {
+            if(program.get(i) instanceof FalseBranchCode)
+            {
+                FalseBranchCode fbc = (FalseBranchCode) program.get(i);
+                if(h.containsKey(fbc.getLabel() + fbc.getNumber()))
+                {
+                    fbc.setNumber(h.get(fbc.getLabel() + fbc.getNumber()));
+                }
+            }
+            else if(program.get(i) instanceof GotoCode)
+            {
+                GotoCode gtc = (GotoCode) program.get(i);
+                if(h.containsKey(gtc.getLabel() + gtc.getNumber()))
+                {
+                    gtc.setNumber(h.get(gtc.getLabel() + gtc.getNumber()));
+                }
+            }
+            else if (program.get(i) instanceof CallCode)
+            {
+                CallCode cc = (CallCode) program.get(i);
+                if(h.containsKey(cc.getLabel() + cc.getNumber()))
+                {
+                    cc.setNumber(h.get(cc.getLabel() + cc.getNumber()));
+                }
+            }
+        }
     }
 
     //this should store a fully initialized bytecode instance
     public void setCode(ByteCode code){
         program.add(code);
     }
-
-
 }
