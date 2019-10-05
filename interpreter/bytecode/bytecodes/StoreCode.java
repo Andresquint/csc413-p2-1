72dc64242407f6a1335a463300b91090a307113f
scrable <cruss.seq11@gmail.com>
2019-10-04 19:53:02 -0700

    change type to string instead of char

--- interpreter/bytecode/bytecodes/StoreCode.java
+++ interpreter/bytecode/bytecodes/StoreCode.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 
 public class StoreCode extends ByteCode {
     private int offset;
-    private char id;
+    private String id;
 
     @Override
     public void initCode(ArrayList args) {
         try {
             offset = Integer.parseInt((String) args.get(1));
             if (args.size() == 3)
-             id = ((String)args.get(2)).charAt(0);
+             id = (String)args.get(2);
         }
         catch(Exception e){e.printStackTrace();}
     }
