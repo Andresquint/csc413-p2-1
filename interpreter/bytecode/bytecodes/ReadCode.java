c3e58c2df10a9ca087c859c7116378a6e62c9c82
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:25:59 -0700

    complete execute functions

--- interpreter/bytecode/bytecodes/ReadCode.java
+++ interpreter/bytecode/bytecodes/ReadCode.java
@@ -3,15 +3,25 @@ package interpreter.bytecode.bytecodes;
 import interpreter.VirtualMachine;
 import interpreter.bytecode.ByteCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class ReadCode extends ByteCode {
     @Override
-    public void initCode(ArrayList args){
+    public void initCode(ArrayList args) {
     }
 
     @Override
     public void execute(VirtualMachine virtualMachine) {
-
+        try {
+            System.out.print("Enter integer: ");
+            BufferedReader inputString = new BufferedReader(new InputStreamReader(System.in));
+            String wholeLine = inputString.readLine();
+            virtualMachine.runStack.push(Integer.parseInt(wholeLine));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
