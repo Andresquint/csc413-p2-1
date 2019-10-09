683f34a26e1b4703c8d9831ea622deb992d23ead
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:25:25 -0700

    complete execute functions

--- interpreter/bytecode/bytecodes/LabelCode.java
+++ interpreter/bytecode/bytecodes/LabelCode.java
@@ -7,22 +7,22 @@ import java.util.ArrayList;
 
 public class LabelCode extends ByteCode {
     private String idName;
-    private int idNumber = 0;
+    private int idNumber;
 
     @Override
     public void initCode(ArrayList args) {
-        idName = (String) args.get(1);
-        if(args.size() == 3)
-            idNumber = Integer.parseInt((String)args.get(2));
+        idName = (String) args.get(0);
     }
 
     @Override
     public void execute(VirtualMachine virtualMachine) {
 
     }
+
     public String getLabel() {
         return idName;
     }
+
     public int getNumber() {
         return idNumber;
     }
