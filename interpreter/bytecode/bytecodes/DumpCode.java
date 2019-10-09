51fbed73e4224945871f95d38eb938ac7c62cc5c
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:24:53 -0700

    complete execute functions

--- interpreter/bytecode/bytecodes/DumpCode.java
+++ interpreter/bytecode/bytecodes/DumpCode.java
@@ -6,15 +6,16 @@ import interpreter.bytecode.ByteCode;
 import java.util.ArrayList;
 
 public class DumpCode extends ByteCode {
-   private boolean state;
+    private boolean state;
 
     @Override
     public void initCode(ArrayList args) {
-        state = ((String) args.get(1)).toLowerCase().equals("on");
+        state = ((String) args.get(0)).toLowerCase().equals("on");
     }
 
     @Override
     public void execute(VirtualMachine virtualMachine) {
 
+        virtualMachine.isDumping = state;
     }
 }
