32230bc4b5fca6265cd34cadc572b6f60e266b37
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:24:47 -0700

    complete execute functions

--- interpreter/bytecode/bytecodes/CallCode.java
+++ interpreter/bytecode/bytecodes/CallCode.java
@@ -6,31 +6,32 @@ import interpreter.bytecode.ByteCode;
 import java.util.ArrayList;
 
 public class CallCode extends ByteCode {
-    private int callValue;
+    private int callAddress;
     private String id;
 
     @Override
     public void initCode(ArrayList args) {
         try {
-            id = (String)args.get(1);
-            if (args.size() == 3)
-                callValue = Integer.parseInt((String) args.get(2));
+            id = (String) args.get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch(Exception e){e.printStackTrace();}
     }
 
     @Override
     public void execute(VirtualMachine virtualMachine) {
+        //push the value first into returnaddrs to keep the place
+        virtualMachine.returnAddrs.push(virtualMachine.pc);
+        //change the program counter to a new location
+        virtualMachine.pc = callAddress;
 
     }
 
     public String getLabel() {
         return id;
     }
-    public void setNumber(int addr){
-        callValue = addr;
-    }
-    public int getNumber() {
-        return callValue;
+
+    public void setNumber(int addr) {
+        callAddress = addr;
     }
 }
