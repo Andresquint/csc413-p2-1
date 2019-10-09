426d29eb9e0c657ea4fc74705665ee15667d7d09
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:29:49 -0700

    make the members public

--- interpreter/VirtualMachine.java
+++ interpreter/VirtualMachine.java
@@ -6,25 +6,29 @@ import java.util.Stack;
 
 public class VirtualMachine {
 
-    private RunTimeStack   runStack;
-    private Stack<Integer> returnAddrs;
-    private Program        program;
-    private int            pc;
-    private boolean        isRunning;
+    public RunTimeStack runStack;
+    public Stack<Integer> returnAddrs;
+    public Program program;
+    public int pc;
+    public boolean isRunning;
+    public boolean isDumping;
 
     protected VirtualMachine(Program program) {
         this.program = program;
     }
 
-    public void executeProgram(){
+    public void executeProgram() {
         pc = 0;
         runStack = new RunTimeStack();
         returnAddrs = new Stack<Integer>();
         isRunning = true;
-        while(isRunning){
+        while (isRunning) {
             ByteCode code = program.getCode(pc);
             code.execute(this);
+            pc++;
         }
-    };
+    }
+
+    ;
 
 }
