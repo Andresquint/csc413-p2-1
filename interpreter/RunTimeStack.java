6060902fdd0d5aea6e5ddc96221586de712bbadf
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:29:14 -0700

    rework each method in runtimestack as there were various out of bounds issues

--- interpreter/RunTimeStack.java
+++ interpreter/RunTimeStack.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 public class RunTimeStack {
 
     private ArrayList<Integer> runTimeStack;
-    private Stack<Integer>     framePointer;
+    private Stack<Integer> framePointer;
 
     public RunTimeStack() {
         runTimeStack = new ArrayList<>();
@@ -15,53 +15,56 @@ public class RunTimeStack {
         // point of our language, so its frame pointer is 0.
         framePointer.add(0);
     }
-    public void dump(){
+
+    public void dump() {
 
     }
+
     public int peek() {
-        if(runTimeStack.size() >= 1)
-            return runTimeStack.get(runTimeStack.size()-1);
-        else return -1;
+        return runTimeStack.get(runTimeStack.size() - 1);
     }
-    public int push(int i){
+
+    public int push(int i) {
         runTimeStack.add(i);
         return i;
     }
-    public int pop(){
-        int i = -1;
-        if (runTimeStack.size() > 0)
-            i = runTimeStack.remove(runTimeStack.size());
+
+    public int pop() {
+        int i = runTimeStack.get(runTimeStack.size() - 1);
+        runTimeStack.remove(runTimeStack.size() - 1);
         return i;
     }
-    public int store(int offset){
-        if(offset + framePointer.peek() > runTimeStack.size() - 1 || offset < 0)
-        {
-            return -1;
-        }
-        else {
-            int p = pop();
-            runTimeStack.set(p, framePointer.peek() + offset);
-            return p;
-        }
-    }
-    public int load(int offset){
-       // if(framePointer.peek() + offset < runTimeStack.size())
-        runTimeStack.add(runTimeStack.get(framePointer.peek() + offset));
-        return runTimeStack.get(framePointer.peek() + offset);
-    }
-    public void newFrameAt(int offset){
-        if(runTimeStack.size() - offset != runTimeStack.get(runTimeStack.size() - 1)){
-            framePointer.add(runTimeStack.size() - offset);
-        }
-    }
-    public void popFrame(){
-        int topValue = runTimeStack.get(runTimeStack.size() - 1);
 
-        runTimeStack.subList(framePointer.peek(), runTimeStack.size());
+    public int store(int offset) {
+        int i = this.pop();
+        runTimeStack.set(framePointer.peek() + offset, i);
+        return i;
+    }
 
-        if(framePointer.peek() != 0)
-            framePointer.pop();
+    public int load(int offset) {
+        int i = runTimeStack.get(framePointer.peek() + offset);
+        runTimeStack.add(i);
+        return i;
+    }
 
-        runTimeStack.add(topValue);
+    public void newFrameAt(int offset) {
+        framePointer.push(this.runTimeStack.size() - offset);
+    }
+
+    public void popFrame() {
+        //get the first value on the frame
+        int topValue = this.peek();
+        //pop this item off the frame pointer
+        int secondValue = framePointer.pop();
+        //run decrementing loop to run from the top of the stack
+        //and run until the desired frame size is reached
+        for (int i = runTimeStack.size() - 1; i >= secondValue; i--)
+            this.pop();
+        //place the old value onto the top of the stack without the previous frame
+        this.push(topValue);
+    }
+
+    public int size() {
+        return runTimeStack.size();
     }
 }
