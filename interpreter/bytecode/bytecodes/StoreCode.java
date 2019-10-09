d645baf7d44dcdf2fb802b0fed51ecfa5e49795e
Corey Russ <cruss.seq11@gmail.com>
2019-10-08 21:26:08 -0700

    complete execute functions

--- interpreter/bytecode/bytecodes/StoreCode.java
+++ interpreter/bytecode/bytecodes/StoreCode.java
@@ -7,20 +7,22 @@ import java.util.ArrayList;
 
 public class StoreCode extends ByteCode {
     private int offset;
+    private int topOfStack;
     private String id;
 
     @Override
     public void initCode(ArrayList args) {
         try {
-            offset = Integer.parseInt((String) args.get(1));
-            if (args.size() == 3)
-             id = (String)args.get(2);
+            offset = Integer.parseInt((String) args.get(0));
+            id = (String) args.get(1);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch(Exception e){e.printStackTrace();}
     }
 
     @Override
     public void execute(VirtualMachine virtualMachine) {
-
+        topOfStack = virtualMachine.runStack.peek();
+        virtualMachine.runStack.store(offset);
     }
 }
